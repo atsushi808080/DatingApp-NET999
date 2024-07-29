41963f0d9876aad2c18d1bb63dc73e6695ea3d43
Neil <neil@mail.tca.org.tw>
2024-07-29 14:18:11 +0800

    DEMO 2

--- Program.cs
+++ Program.cs
@@ -3,9 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();//註冊Cors服務
+
 var app = builder.Build();
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+//中介軟體設定該服務允許任何http頭部 任何http方法並且指定網域才可進入
 app.MapControllers();
 app.Run();
